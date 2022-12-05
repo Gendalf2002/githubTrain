6cef54e22f9b3d436fdb8a4bdbd150ff19a26688
Gendalf228 <pronko_k@inbox.ru>
2022-12-05 22:12:26 +0300

    data types 2

--- src/com/gendalf/firstproj/Main.java
+++ src/com/gendalf/firstproj/Main.java
@@ -13,22 +13,33 @@ public class Main {
         float b = 5.9f;
         double c = 5.9;
         System.out.println(b + c);
-        boolean l = true;
+
         boolean fas = false;
-        System.out.println(l + " " + fas);
+
         System.out.println(a != f);
         char lll = 98;
         System.out.println(lll);
         int ggg = lll;
         System.out.println(ggg);
-        if (b==c || b==c){
+        if (b == c || b == c) {
             System.out.println("ровно");
-        } else if(1>2){
+        } else if (1 > 2) {
             System.out.println("не ровно");
-        }else{
+        } else {
             System.out.println("geg");
-
         }
+        int v = 2;
+        int l = 3;
+        int n = 5;
+        if (v > l && v > n) {
+            System.out.println(v);
+        } else if (l > v && l > n) {
+            System.out.println(l);
+        } else if (n > v && n > l) {
+            System.out.println(n);
+        }
+
+
         // Найти большее из 3 чисел
         // Из заглавной сделать мальенькую,из маленькой заглавную
         // из 4 знач числа вывести 2 последние цифры
