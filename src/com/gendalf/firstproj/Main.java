4dd8d412f87c2d89b3b044fdf2e837544528ed2b
Gendalf228 <pronko_k@inbox.ru>
2022-12-05 21:24:58 +0300

    data types

--- src/com/gendalf/firstproj/Main.java
+++ src/com/gendalf/firstproj/Main.java
@@ -4,5 +4,24 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        int a = 2;
+        short f = 2;
+        byte g = 17;
+        long t = 1111;
+        System.out.println(a);
+        System.out.println(a+" "+a);
+        float b = 5.9f;
+        double c = 5.9;
+        System.out.println(b + c);
+        boolean l = true;
+        boolean fas = false;
+        System.out.println(l + " " + fas);
+        System.out.println(a!=f);
+        char lll = 98;
+        System.out.println(lll);
+        int ggg = lll;
+        System.out.println(ggg);
+
+
     }
 }
