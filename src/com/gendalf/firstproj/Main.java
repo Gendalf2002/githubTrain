98606b279bccd208e65a0d79e4465cc33330be50
Gendalf228 <pronko_k@inbox.ru>
2022-12-06 00:07:11 +0300

    data types 2

--- src/com/gendalf/firstproj/Main.java
+++ src/com/gendalf/firstproj/Main.java
@@ -4,30 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        int a = 2;
-        short f = 2;
-        byte g = 17;
-        long t = 1111;
-        System.out.println(a);
-        System.out.println(a + " " + a);
-        float b = 5.9f;
-        double c = 5.9;
-        System.out.println(b + c);
-
-        boolean fas = false;
-
-        System.out.println(a != f);
-        char lll = 98;
-        System.out.println(lll);
-        int ggg = lll;
-        System.out.println(ggg);
-        if (b == c || b == c) {
-            System.out.println("ровно");
-        } else if (1 > 2) {
-            System.out.println("не ровно");
-        } else {
-            System.out.println("geg");
-        }
+        //int a = 2;
+        //short f = 2;
+        //byte g = 17;
+        //long t = 1111;
+        //System.out.println(a);
+//        System.out.println(a + " " + a);
+//        float b = 5.9f;
+//        double c = 5.9;
+//        System.out.println(b + c);
+//
+//        boolean fas = false;
+//
+//        System.out.println(a != f);
+//        char lll = 98;
+//        System.out.println(lll);
+//       int ggg = lll;
+//        System.out.println(ggg);
+//        if (b == c || b == c) {
+//            System.out.println("ровно");
+//        } else if (1 > 2) {
+//            System.out.println("не ровно");
+//        } else {
+//            System.out.println("geg");
+//        }
         int v = 2;
         int l = 3;
         int n = 5;
@@ -38,11 +38,27 @@ public class Main {
         } else if (n > v && n > l) {
             System.out.println(n);
         }
+        char p = 72;{
+            System.out.println(p);
+        }
+        int k = p;{
+            System.out.println(k);
+        }
+
+
+
+
+
+
+
+
+        }
+
+
+        ;
 
 
         // Найти большее из 3 чисел
         // Из заглавной сделать мальенькую,из маленькой заглавную
         // из 4 знач числа вывести 2 последние цифры
-
-    }
 }
