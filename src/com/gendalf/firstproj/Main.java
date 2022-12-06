7a85b65d714493337da4d9560b601a5440eefeff
Gendalf228 <pronko_k@inbox.ru>
2022-12-06 20:28:00 +0300

    data types 2

--- src/com/gendalf/firstproj/Main.java
+++ src/com/gendalf/firstproj/Main.java
@@ -3,69 +3,81 @@ package com.gendalf.firstproj;
 import java.util.Scanner;
 
 public class Main {
+    public static void findLustNumber(int k) {
+        k = k % 100;
+        System.out.println(k);
+    }
 
     public static void main(String[] args) {
-        // write your code here
-        //int a = 2;
-        //short f = 2;
-        //byte g = 17;
-        //long t = 1111;
-        //System.out.println(a);
-//        System.out.println(a + " " + a);
-//        float b = 5.9f;
-//        double c = 5.9;
-//        System.out.println(b + c);
-//
-//        boolean fas = false;
-//
-//        System.out.println(a != f);
-//        char lll = 98;
-//        System.out.println(lll);
-//       int ggg = lll;
-//        System.out.println(ggg);
-//        if (b == c || b == c) {
-//            System.out.println("ровно");
-//        } else if (1 > 2) {
-//            System.out.println("не ровно");
-//        } else {
-//            System.out.println("geg");
-//        }
+//        countNumbers(0,10000);
+//        printAlphabet();
+        power(2,4);
+//        //1
 //        int v = 2;
 //        int l = 3;
 //        int n = 5;
-//        if (v > l && v > n) {
-//            System.out.println(v);
-//        } else if (l > v && l > n) {
-//            System.out.println(l);
-//        } else if (n > v && n > l) {
-//            System.out.println(n);
-//        }
-//        char p = 65;
-//        System.out.println(p);
-//        char dd = 97;
-//        System.out.println(dd);
-//        System.out.println(dd + 32);
+//        int max = findMaxNumber(v, l, n);
+//        System.out.println(max);
+//
+//        //2
 //        Scanner scanner = new Scanner(System.in);
 //        char ccc = scanner.next().charAt(0);
-//        int a = ccc;
-//        if (65 < a && a < 90) {
-//            a += 32;
-//            System.out.println((char) a);
-//        } else if (97 < a && a < 122) {
-//            a -= 32;
-//            System.out.println((char) a);
-//      }
-        int k = 1280 % 100;
-        System.out.println(k);
-
-
-
+//        System.out.println(changeSize(ccc));
+//
+//        //3
+//        int k = scanner.nextInt();
+//        findLustNumber(k);
     }
 
+    public static int findMaxNumber(int g, int y, int pp) {
+        int max = Integer.MIN_VALUE;
+        if (g > pp && g > y) {
+            max = g;
+        } else if (pp > g && pp > y) {
+            max = pp;
+        } else if (y > g && y > pp) {
+            max = y;
+        }
+        return max;
+    }
+
+    public static char changeSize(char ll) {
+        int a = ll;
+        if (65 < a && a < 90) {
+            a += 32;
+        } else {
+            a -= 32;
+
+        }
+        return (char) a;
+    }
+
+    public static void countNumbers(int start, int end) {
+        int soum = 0;
+        for (int i = start; i <= end; i++) {
+            soum =soum + i;
+            System.out.println(i+" "+soum);
+        }
+    }
+    public static void printAlphabet(){
+        int a = 65;
+        int o = 97;
+        for(int i = 0;i<26;i++){
+            System.out.println((char)(a+i)+" "+(char)(o+i)  );
+        }
+    }
+    public static void power(int a,int pow){
+        int result = a;
+        for (int i = 1;i<=pow;i++){
+            System.out.println(result);
+            result = a * result ;
+        }
+    }
+    //таблица умножения на 3
+    //найти кол-во отрицательных чисел , ввод с клавиатуры
+    // 3%-вклад положили s руб . какой станет сумма вклада через n-лет
 }
 
 
-// Найти большее из 3 чисел
-// Из заглавной сделать мальенькую,из маленькой заглавную
-// из 4 знач числа вывести 2 последние цифры
+
 
